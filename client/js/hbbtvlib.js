2ebd9aab1752832e53c138b1ad5506b9810dadcf
robinjimenez <robin.jimenez.2015@salleurl.edu>
2018-11-10 20:12:19 +0100

    Update client/js/hbbtvlib.js

--- client/js/hbbtvlib.js
+++ client/js/hbbtvlib.js
@@ -31,40 +31,4 @@ function createVideoPlayer(url) {
   player.autoplay = true;
 
   document.getElementById("video-container").appendChild(player);
-=======
-			player.setFullScreen(true);
-			player.play(1);
-		}
-
-/**
- * HTML5 video tag can be used only in HbbTV 2.0 profile
- * @param url
- */
-function createVideoPlayer(url) {
-	var player, source;
-
-	player = document.createElement("video");
-	source = document.createElement("source");
-	source.src = url;
-	source.type = "video/mp4";
-	player.appendChild(source);
-
-	player.autoplay = true;
-
-	document.body.getElementById("video-container").appendChild(player);
-}
-
-
-//El player no se si funciona o no
-
-function createPlayer (video_id) {
-  var parsedURL;
-
-  if (parsedURL.type === "html5" && parseFloat(navigator.userAgent.substring(navigator.userAgent.indexOf("HbbTV/")+8, navigator.userAgent.indexOf(" ", navigator.userAgent.indexOf("HbbTV/")))) >= 3.1) {
-    createVideoPlayer(parsedURL.url);
-  } else {
-    createAVPlayer(parsedURL.url);
-  }
-
-  controls.initialize();
 }
